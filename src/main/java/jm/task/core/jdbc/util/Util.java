0952216b14dd8df659e77a30c838b5525f981ab8
ya1789 <ya1789@mail.ru>
2023-01-17 09:37:00 +0300

    Fixed UserDaoJDBCImpl.class and Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,46 +1,25 @@
 package jm.task.core.jdbc.util;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-
 
 public class Util {
+    private static final String url = "jdbc:mysql://localhost:3306/usersdb";
+    private static final String user = "root";
+    private static final String password = "htg1165";
     private static Connection connection;
 
     private Util() {
 
     }
 
-    private static class UtilHolder {
-        public static final Util UTIL_INSTANCE = new Util();
-    }
-
-    public static Util getInstance() {
-        return UtilHolder.UTIL_INSTANCE;
-    }
-
     public static Connection getConnection() {
-        Properties properties = getProps();
         try {
-            connection = DriverManager.getConnection(properties.getProperty("db.url"), properties.getProperty("db.user"),properties.getProperty("db.password"));
+            connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return connection;
-    }
-
-    private static Properties getProps() {
-        Properties properties = new Properties();
-        try (InputStream inputStream = new FileInputStream("src/main/resources/app.properities")) {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return properties;
     }
 }
